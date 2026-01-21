085c0027c459a073d81f4a1ee3816da1692fdc7c
atraung98-del <atraung98@gmail.com>
2026-01-21 20:15:34 +0900

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,6 @@
+const gitPro=document.getElementById('gitPro');
+const topt=document.getElementById('t-opt');
+gitPro.addEventListener('click',e=>{
+    e.preventDefault();
+    window.location.href="https://github.com/atraung98-del/JPcourses";
+});
