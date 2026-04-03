72acd3766c6a4ce208b5459dfce5674d66148262
atraung98-del <atraung98@gmail.com>
2026-04-03 23:17:39 +0900

    Delete homepage.js

--- homepage.js
+++
@@ -1,6 +0,0 @@
-const gitPro=document.getElementById('gitPro');
-const topt=document.getElementById('t-opt');
-gitPro.addEventListener('click',e=>{
-    e.preventDefault();
-    window.location.href="https://github.com/atraung98-del/JPcourses";
-});
